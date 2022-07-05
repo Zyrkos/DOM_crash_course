b7b02ecb10536437f1ed1c9f37ce225134c2bcc0
Zyrkos <pedrocastro1995@hotmail.com>
2022-07-05 16:52:36 +0100

    .

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const form = document.getElementById('addForm');
 const itemList = document.getElementById('items');
+const filter = document.getElementById('filter');
 
 //Form submit event
 form.addEventListener('submit', addItem);
+
+//delete event
+itemList.addEventListener('click', removeItem);
+
+//filter event
+filter.addEventListener('keyup', filterItems);
 
 //Add item
 function addItem(e){
@@ -31,4 +38,33 @@ function addItem(e){
     itemList.append(li);
 
 
+}
+
+function removeItem(e){
+    //we want the event to only delete when we press the X, not the item itself
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are you sure?')) {
+            const li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+    
+}
+
+//filter items
+function filterItems(e) {
+    //convert text to lowercase
+    const text = e.target.value.toLowerCase();
+    //get LIs
+    const items = itemList.getElementsByTagName('li');
+    //convert to an array
+    Array.from(items).forEach(function(item){
+        const itemName = item.firstChild.textContent;
+        //We then want to compare if the input of the search bar is equal to the values in the list
+        if(itemName.toLowerCase().indexOf(text) != -1) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    })
 }
