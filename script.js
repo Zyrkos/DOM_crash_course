98d9bbb9aeeb23b8822ff2196572b26cdb197b41
Zyrkos <pedrocastro1995@hotmail.com>
2022-07-05 14:56:50 +0100

    index.html

--- script.js
+++ script.js
@@ -3,3 +3,10 @@
 const itemList = document.querySelector('#items');
 itemList.parentNode.style.backgroundColor = '#f4f4f4';
 
+
+const button = document.getElementById('button').addEventListener('click', buttonClick);
+
+function buttonClick(){
+    
+}
+
