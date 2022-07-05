64ed595932bfa63c888cef46f913c660ba9f5cc6
Zyrkos <pedrocastro1995@hotmail.com>
2022-07-05 16:25:33 +0100

    .

--- script.js
+++ script.js
@@ -1,48 +1,12 @@
+const form = document.getElementById('addForm');
+const itemList = document.getElementById('items');
 
+//Form submit event
+form.addEventListener('submit', addItem);
 
-//EVENTS
+//Add item
+function addItem(e){
+    e.preventDefault();
 
-
-/* const button = document.getElementById('button').addEventListener('click', buttonClick);
-
-function buttonClick(){
-} */
-
-
-const button = document.getElementById('button');
-const box = document.getElementById('box');
-
-//GETTING the position of the MOUSE and do things with it:
-//box.addEventListener('mouseover', runEvent);
-
-
-//GETING input from KEYDOWN event
-/* const itemInput = document.querySelector('input[type="text"]' );
-const form = document.querySelector('form'); */
-
-/* itemInput.addEventListener('keydown', runEvent);
-itemInput.addEventListener('keyup', runEvent);
-itemInput.addEventListener('keypress', runEvent); */
-
-
-itemInput.addEventListener('focus', runEvent);
-itemInput.addEventListener('blur', runEvent);
-
-
-
-
-
-
-function runEvent(e){
-    console.log('EVENT TYPE: '+e.type);
-    
-    //document.getElementById('output').innerHTML = '<h3>' +e.target.value+'</h3>';
-
-    //TRACKS mouse movement and displays values on "output"//
-    //output.innerHTML = '<h3>MouseX: '+e.offsetX+' </h3><h3>MouseY: '+e.offsetY+'</h3>';
-
-
-    //CHANGES color of the box based on the position of the mouse
-    //box.style.backgroundColor = "rgb("+e.offsetX+","+e.offsetY+", 40)";
+    console.log(1);
 }
-
